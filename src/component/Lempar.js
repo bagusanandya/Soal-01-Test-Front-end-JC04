b1f2760bc7caecc18f015712598b3142514dfe12
bagusanandya <35951272+bagusanandya@users.noreply.github.com>
2018-03-07 20:27:54 +0700

    Add files via upload

---
+++ src/component/Lempar.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+class Lempar extends Component{
+    render(){
+        return(
+            <h1>{this.props.masuk}</h1>
+        )
+    }
+}
+export default Lempar
